0a7df4814b6f07c890f223b1237fd72b4c588afe
VMstation1 <vmstation1@vmstation1s-mac.local>
2016-07-21 12:26:39 +0800

    Initial Commit

---
+++ gitDemoTH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitDemoTH
+//
+//  Created by VMstation1 on 21/07/2016.
+//  Copyright © 2016 VMstation1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
